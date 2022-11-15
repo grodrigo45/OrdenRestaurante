d2825ec9431469ea756838370cedcd2ed6a614cc
gerardoJoseVilledaErazo <gerardovilleda@gmail.com>
2022-11-15 07:03:02 -0600

    agregando animationdrawable a ChooseOne Activity

--- app/src/main/java/com/example/ordenrestaurante/ChooseOne.java
+++ app/src/main/java/com/example/ordenrestaurante/ChooseOne.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -14,10 +15,25 @@ public class ChooseOne extends AppCompatActivity {
     String type;
     ConstraintLayout bgimage;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_one);
+
+
+        AnimationDrawable animationDrawable = new AnimationDrawable();
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.bg2),3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img2),3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img3),3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img4),3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img5),3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img6),3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img7),3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img8),3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.bg3),3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img9),3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img10),3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img11),3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img11),3000);
     }
 }
