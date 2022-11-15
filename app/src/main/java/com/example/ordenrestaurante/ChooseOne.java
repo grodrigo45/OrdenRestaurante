400f7fed1bc8fdb7352bec9257f7ed19449a35e2
gerardoJoseVilledaErazo <gerardovilleda@gmail.com>
2022-11-15 12:12:12 -0600

    se agregaron los setOnClickListener tanto para Chef Customer y DeliveryPerson en ChooseOne Activity

--- app/src/main/java/com/example/ordenrestaurante/ChooseOne.java
+++ app/src/main/java/com/example/ordenrestaurante/ChooseOne.java
@@ -6,6 +6,7 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class ChooseOne extends AppCompatActivity {
@@ -51,8 +52,69 @@ public class ChooseOne extends AppCompatActivity {
         DeliveryPerson = (Button)findViewById(R.id.repartidor);/// se reemplazo delivery por repartido
         Customer = (Button)findViewById(R.id.cliente);/// se reemplazo customer por cliente
 
+        Chef.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(type.equals("Email")){
+                    Intent loginemail  = new Intent(ChooseOne.this,Cheflogin.class);
+                    startActivity(loginemail);
+                    finish();
+                }
+                if(type.equals("Phone")){
+                    Intent loginphone  = new Intent(ChooseOne.this,Chefloginphone.class);
+                    startActivity(loginphone);
+                    finish();
+                }
+                if(type.equals("SignUp")){
+                    Intent Register  = new Intent(ChooseOne.this,ChefRegistration.class);
+                    startActivity(Register);
+                }
+            }
+        });
 
+        Customer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                if(type.equals("Email")){
+                    Intent loginemailcust  = new Intent(ChooseOne.this,Login.class);
+                    startActivity(loginemailcust);
+                    finish();
+                }
+                if(type.equals("Phone")){
+                    Intent loginphonecust  = new Intent(ChooseOne.this,Loginphone.class);
+                    startActivity(loginphonecust);
+                    finish();
+                }
+                if(type.equals("SignUp")){
+                    Intent Registercust  = new Intent(ChooseOne.this,Registration.class);
+                    startActivity(Registercust);
+                }
+
+            }
+        });
+
+        DeliveryPerson.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(type.equals("Email")){
+                    Intent loginemail = new Intent(ChooseOne.this,Delivery_Login.class);
+                    startActivity(loginemail);
+                    finish();
+                }
+                if(type.equals("Phone")){
+                    Intent loginphone  = new Intent(ChooseOne.this,Delivery_Loginphone.class);
+                    startActivity(loginphone);
+                    finish();
+                }
+                if(type.equals("SignUp")){
+                    Intent Registerdelivery  = new Intent(ChooseOne.this,Delivery_Registration.class);
+                    startActivity(Registerdelivery);
+                }
+
+            }
+        });
 
     }
 }
