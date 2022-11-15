f800d6e4d1ba7da2e76c452eace6a7ccfdd22a7e
gerardoJoseVilledaErazo <gerardovilleda@gmail.com>
2022-11-15 06:57:18 -0600

    inicializando botones Chef, Customer DeliveryPerson, intent, string type, constraintlayout bgimage ChooseOne Activity

--- app/src/main/java/com/example/ordenrestaurante/ChooseOne.java
+++ app/src/main/java/com/example/ordenrestaurante/ChooseOne.java
@@ -1,10 +1,19 @@
 package com.example.ordenrestaurante;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ChooseOne extends AppCompatActivity {
+
+    Button Chef, Customer, DeliveryPerson;
+    Intent intent;
+    String type;
+    ConstraintLayout bgimage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
