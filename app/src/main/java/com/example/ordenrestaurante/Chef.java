c7b7413b9cc487021b0812a77e4a5760cff67258
gerardoJoseVilledaErazo <gerardovilleda@gmail.com>
2022-11-15 12:20:23 -0600

    se agregaron las activitys Cheflogin Chefloginphone ChefRegistration y Login

---
+++ app/src/main/java/com/example/ordenrestaurante/Chef.java
@@ -0,0 +1,13 @@
+package com.example.ordenrestaurante;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Chef extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
