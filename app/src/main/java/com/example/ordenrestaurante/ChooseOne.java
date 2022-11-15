b7d83615e6f7a6fa505272cb6736b1363eadd5ce
gerardoJoseVilledaErazo <gerardovilleda@gmail.com>
2022-11-15 16:14:01 -0600

    se agrego todo el contenido a la activity choose_one y se modificaron algunas cosas en la activity ChooseOnejava

--- app/src/main/java/com/example/ordenrestaurante/ChooseOne.java
+++ app/src/main/java/com/example/ordenrestaurante/ChooseOne.java
@@ -23,19 +23,19 @@ public class ChooseOne extends AppCompatActivity {
 
 
         AnimationDrawable animationDrawable = new AnimationDrawable();
-        animationDrawable.addFrame(getResources().getDrawable(R.drawable.bg2),3000);
-        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img2),3000);
-        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img3),3000);
-        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img4),3000);
-        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img5),3000);
-        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img6),3000);
-        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img7),3000);
-        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img8),3000);
-        animationDrawable.addFrame(getResources().getDrawable(R.drawable.bg3),3000);
-        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img9),3000);
-        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img10),3000);
-        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img11),3000);
-        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img11),3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.bg2), 3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img2), 3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img3), 3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img4), 3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img5), 3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img6), 3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img7), 3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img8), 3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.bg3), 3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img9), 3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img10), 3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img11), 3000);
+        animationDrawable.addFrame(getResources().getDrawable(R.drawable.img11), 3000);
 
         animationDrawable.setOneShot(false);
         animationDrawable.setEnterFadeDuration(850);
@@ -48,25 +48,25 @@ public class ChooseOne extends AppCompatActivity {
         intent = getIntent();
         type = intent.getStringExtra("Home").toString().trim();
 
-        Chef = (Button)findViewById(R.id.chef);
-        DeliveryPerson = (Button)findViewById(R.id.repartidor);/// se reemplazo delivery por repartido
-        Customer = (Button)findViewById(R.id.cliente);/// se reemplazo customer por cliente
+        Chef = (Button) findViewById(R.id.chef);
+        DeliveryPerson = (Button) findViewById(R.id.delivery);
+        Customer = (Button) findViewById(R.id.customer);
 
         Chef.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(type.equals("Email")){
-                    Intent loginemail  = new Intent(ChooseOne.this,Cheflogin.class);
+                if (type.equals("Email")) {
+                    Intent loginemail = new Intent(ChooseOne.this, Cheflogin.class);
                     startActivity(loginemail);
                     finish();
                 }
-                if(type.equals("Phone")){
-                    Intent loginphone  = new Intent(ChooseOne.this,Chefloginphone.class);
+                if (type.equals("Phone")) {
+                    Intent loginphone = new Intent(ChooseOne.this, Chefloginphone.class);
                     startActivity(loginphone);
                     finish();
                 }
-                if(type.equals("SignUp")){
-                    Intent Register  = new Intent(ChooseOne.this,ChefRegistration.class);
+                if (type.equals("SignUp")) {
+                    Intent Register = new Intent(ChooseOne.this, ChefRegistration.class);
                     startActivity(Register);
                 }
             }
@@ -76,18 +76,18 @@ public class ChooseOne extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                if(type.equals("Email")){
-                    Intent loginemailcust  = new Intent(ChooseOne.this,Login.class);
+                if (type.equals("Email")) {
+                    Intent loginemailcust = new Intent(ChooseOne.this, Login.class);
                     startActivity(loginemailcust);
                     finish();
                 }
-                if(type.equals("Phone")){
-                    Intent loginphonecust  = new Intent(ChooseOne.this,Loginphone.class);
+                if (type.equals("Phone")) {
+                    Intent loginphonecust = new Intent(ChooseOne.this, Loginphone.class);
                     startActivity(loginphonecust);
                     finish();
                 }
-                if(type.equals("SignUp")){
-                    Intent Registercust  = new Intent(ChooseOne.this,Registration.class);
+                if (type.equals("SignUp")) {
+                    Intent Registercust = new Intent(ChooseOne.this, Registration.class);
                     startActivity(Registercust);
                 }
 
@@ -98,18 +98,18 @@ public class ChooseOne extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                if(type.equals("Email")){
-                    Intent loginemail = new Intent(ChooseOne.this,Delivery_Login.class);
+                if (type.equals("Email")) {
+                    Intent loginemail = new Intent(ChooseOne.this, Delivery_Login.class);
                     startActivity(loginemail);
                     finish();
                 }
-                if(type.equals("Phone")){
-                    Intent loginphone  = new Intent(ChooseOne.this,Delivery_Loginphone.class);
+                if (type.equals("Phone")) {
+                    Intent loginphone = new Intent(ChooseOne.this, Delivery_Loginphone.class);
                     startActivity(loginphone);
                     finish();
                 }
-                if(type.equals("SignUp")){
-                    Intent Registerdelivery  = new Intent(ChooseOne.this,Delivery_Registration.class);
+                if (type.equals("SignUp")) {
+                    Intent Registerdelivery = new Intent(ChooseOne.this, Delivery_Registration.class);
                     startActivity(Registerdelivery);
                 }
 
