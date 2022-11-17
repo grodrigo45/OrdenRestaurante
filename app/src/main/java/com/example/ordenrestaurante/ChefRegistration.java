b9bf8f14186e997d7ffadd3994f729c9b6809868
gerardoJoseVilledaErazo <gerardovilleda@gmail.com>
2022-11-17 08:38:46 -0600

    ChefRegistration.java observacion ReusableCodeForAll esta marcado en rojo

--- app/src/main/java/com/example/ordenrestaurante/ChefRegistration.java
+++ app/src/main/java/com/example/ordenrestaurante/ChefRegistration.java
@@ -187,7 +187,7 @@ public class ChefRegistration extends AppCompatActivity {
                                                                 } else {
                                                                     mDialog.dismiss();
 
-//                                                                    ReusableCodeForAll.ShowAlert(ChefRegistration.this, "Error", task.getException().getMessage());
+                                                                    ReusableCodeForAll.ShowAlert(ChefRegistration.this, "Error", task.getException().getMessage());
                                                                 }
                                                             }
                                                         });
