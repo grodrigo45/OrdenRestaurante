470e6101c1c78e84b1f286c7c295613e34aa2511
gerardoJoseVilledaErazo <gerardovilleda@gmail.com>
2022-11-30 14:15:33 -0600

    Part-18 finalizada: se agrego ChefHomeAdapter UpdateDelete_Dish y el item chefmenu_update_delete y se modifico el ChefHomeFragment

--- app/src/main/java/com/example/ordenrestaurante/chefFoodPanel/ChefHomeFragment.java
+++ app/src/main/java/com/example/ordenrestaurante/chefFoodPanel/ChefHomeFragment.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -14,12 +16,29 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.ordenrestaurante.Models.Chef;
+import com.example.ordenrestaurante.Models.UpdateDishModel;
 import com.example.ordenrestaurante.PMenu;
 import com.example.ordenrestaurante.R;
+import com.example.ordenrestaurante.adapter.ChefHomeAdapter;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ChefHomeFragment extends Fragment {
+
+    RecyclerView recyclerView;
+    private List<UpdateDishModel> updateDishModelList;
+    private ChefHomeAdapter adapter;
+    DatabaseReference dataa;
+    private String State, City, Area;
 
     @Nullable
     @Override
@@ -28,8 +47,53 @@ public class ChefHomeFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_chef_home, null);
         getActivity().setTitle("Home");
         setHasOptionsMenu(true);
+
+        recyclerView = v.findViewById(R.id.recycle_menu);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        updateDishModelList = new ArrayList<>();
+        String userid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        dataa = FirebaseDatabase.getInstance().getReference("Chef").child(userid);
+        dataa.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Chef cheff = snapshot.getValue(Chef.class);
+                State = cheff.getState();
+                City = cheff.getCity();
+                Area = cheff.getArea();
+                chefDishes();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
         return v;
 //        return inflater.inflate(R.layout.fragment_chef_home, container, false);
+    }
+
+    private void chefDishes() {
+
+        String useridd = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("FoodDetails").child(State).child(City).child(Area).child(useridd);
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                updateDishModelList.clear();
+                for (DataSnapshot snapshot1 : snapshot.getChildren()) {
+                    UpdateDishModel updateDishModel = snapshot1.getValue(UpdateDishModel.class);
+                    updateDishModelList.add(updateDishModel);
+                }
+                adapter = new ChefHomeAdapter(getContext(), updateDishModelList);
+                recyclerView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     @Override
